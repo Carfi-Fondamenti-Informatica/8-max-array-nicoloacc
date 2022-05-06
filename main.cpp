fc734861f1bbda0cd2de09e60b58a002f04e0ebd
nicoloacc <100343394+nicoloacc@users.noreply.github.com>
2022-05-06 13:42:06 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int y;
+    cin >> y;
+    float x [y];
+    for (int j=0; j<y; j++) {
+        cin >> x [j];
+    }
+    cout << calcolo (x, y ) << endl;
   return 0;
 }
