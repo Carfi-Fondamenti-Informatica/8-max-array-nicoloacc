10dff8f2e57583df6607b43e182053e2add7245c
nicoloacc <100343394+nicoloacc@users.noreply.github.com>
2022-05-06 13:46:42 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,10 +1,10 @@
 #include "lib.h"
 
-float calcolo (float a [], int b) {
+float calcolo (float a [], int b){
     float r=a[0];
-    for (int i=1; i<b;i++) {
-        if (r< a [i]) {
-            r= a [i];
+    for (int i=1; i<b;i++){
+        if (r < a [i]) {
+            r = a [i];
         }
     } return r;
 }
